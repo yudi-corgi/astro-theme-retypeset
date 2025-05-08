979f36a7967f467e8205d30f5c435608a5ebaa40
radishzzz <shuyinsen@gmail.com>
2025-05-08 18:13:15 +0100

    perf: optimize katex config, implement async css loading for katex, improve scrollbar and lightbox loading logic

--- astro.config.ts
+++ astro.config.ts
@@ -18,6 +18,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 
 const url = themeConfig.site.url
 const locale = themeConfig.global.locale
+const isKatexEnabled = themeConfig.global.katex
 const linkPrefetch = themeConfig.preload.linkPrefetch
 const imageHostURL = themeConfig.preload.imageHostURL
 const imageConfig = imageHostURL
@@ -30,6 +31,28 @@ const imageConfig = imageHostURL
       },
     }
   : {}
+
+const remarkPlugins = [
+  remarkDirective,
+  ...(isKatexEnabled ? [remarkMath] : []),
+  remarkAdmonitions,
+  remarkGithubCard,
+  remarkReadingTime,
+]
+
+const rehypePlugins = [
+  rehypeSlug,
+  ...(isKatexEnabled ? [rehypeKatex] : []),
+  rehypeImgToFigure,
+  [
+    rehypeExternalLinks,
+    {
+      target: '_blank',
+      rel: ['nofollow', 'noopener', 'noreferrer', 'external'],
+      protocols: ['http', 'https', 'mailto'],
+    },
+  ],
+] as any[]
 
 export default defineConfig({
   site: url,
@@ -61,26 +84,8 @@ export default defineConfig({
     robotsTxt(),
   ],
   markdown: {
-    remarkPlugins: [
-      remarkDirective,
-      remarkMath,
-      remarkAdmonitions,
-      remarkGithubCard,
-      remarkReadingTime,
-    ],
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeKatex,
-      rehypeImgToFigure,
-      [
-        rehypeExternalLinks,
-        {
-          target: '_blank',
-          rel: ['nofollow', 'noopener', 'noreferrer', 'external'],
-          protocols: ['http', 'https', 'mailto'],
-        },
-      ],
-    ],
+    remarkPlugins,
+    rehypePlugins,
     shikiConfig: {
       // Available themes: https://shiki.style/themes
       themes: {
