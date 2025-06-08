beaa9b056c1bb36a9a1d9724c93df50a8b0b9c1a
radishzzz <shuyinsen@gmail.com>
2025-06-08 12:21:33 +0100

    chore: remove link wrapper component to simplify footer link styling

--- uno.config.ts
+++ uno.config.ts
@@ -58,6 +58,7 @@ export default defineConfig({
     'uno-desktop-column': 'fixed right-[max(5rem,calc(50vw-35rem))] w-14rem',
     'uno-decorative-line': 'mb-4.5 h-0.25 w-10 bg-secondary/25 lg:(mb-6 w-11)',
     'uno-round-border': 'border border-secondary/5 rounded border-solid',
+    'uno-footer-link': 'highlight-hover py-0.8 transition-colors after:bottom-0.35em hover:c-primary',
   },
   variants: [
     (matcher) => {
