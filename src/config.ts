2521fce85c33fa34eae64f32e9373456ad56fd22
radishzzz <shuyinsen@gmail.com>
2025-06-25 22:52:08 +0100

    test: twikoo failed to initialize

--- src/config.ts
+++ src/config.ts
@@ -92,14 +92,14 @@ export const themeConfig: ThemeConfig = {
     // twikoo
     // https://twikoo.js.org/en/
     twikoo: {
-      envId: '',
+      envId: 'https://comment-twikoo-three.vercel.app',
       region: '',
     },
     // waline
     // https://waline.js.org/en/
     waline: {
       // server url
-      serverURL: 'https://retypeset-comment.radishzz.cc',
+      // serverURL: 'https://retypeset-comment.radishzz.cc',
       // emoji url
       emoji: [
         'https://unpkg.com/@waline/emojis@1.2.0/tw-emoji',
