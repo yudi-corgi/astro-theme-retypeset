e945dd9d6c38298f4769782bf2ab2118cdc9eb2a
yudi-corgi <corgi@gmail.com>
2025-08-28 11:49:57 +0800

    feat: Add sitemap.

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const themeConfig: ThemeConfig = {
     // author name
     author: 'YUDI-Corgi',
     // site url
-    url: 'https://blog.yudi-corgi.site',
+    url: 'https://yudi-corgi.site',
     // base path
     // root directory for all pages and assets
     base: '/', // e.g., '/blog', '/docs'
